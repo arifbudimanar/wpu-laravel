aba252a9d347627ff2bcd9c3d8c49c3b61aadc78
arifbudimanar <arifbudiman01041996@hotmail.com>
2022-10-04 16:02:34 +0700

    struktur folder, route dan view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view(
+        'about',
+        [
+            "name" => "Arif Budiman Arrosyid",
+            "email" => "arifbudimanarrosyid@gmail.com",
+            "image" => "logo.png"
+        ]
+    );
+});
+
+Route::get('/blog', function () {
+    return view('posts');
+});
+
+Route::get('/categories', function () {
+    return view('categories');
 });
