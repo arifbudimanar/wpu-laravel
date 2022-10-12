c84046e421c21e68d8b269b8194084ae714e7924
arifbudimanar <arifbudiman01041996@hotmail.com>
2022-10-12 15:58:39 +0700

    post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,50 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
+    use HasFactory;
 
-            'title' => 'Blog kesatu',
-            'slug' => 'blog-kesatu',
-            'author' => 'Arif Budiman Arrosyid',
-            'body' => 'Est minim ea excepteur velit culpa mollit magna ullamco ut aute Lorem adipisicing. Mollit aliquip laboris irure quis adipisicing velit. Proident duis nostrud ex enim enim Lorem et aliqua officia dolor. Duis officia nulla qui ullamco quis minim esse minim. Nisi dolor reprehenderit laborum id amet in nostrud eiusmod id excepteur aute amet.Magna incididunt anim eu in culpa culpa laborum elit aute nostrud sit. Deserunt ea elit velit exercitation. Sint occaecat quis sint ex cillum magna est duis id. Culpa ipsum dolor aliqua officia duis eiusmod laborum.Mollit cupidatat proident cupidatat ex irure id est aute et do eu deserunt minim mollit. Ea tempor amet eiusmod sint tempor officia adipisicing laboris mollit eu. Pariatur in ullamco proident eiusmod ea officia eiusmod magna duis do culpa laborum non mollit.'
-        ],
-        [
-            'title' => 'Blog kedua',
-            'slug' => 'blog-kedua',
-            'author' => 'Arif Budiman Arrosyid',
-            'body' => 'Adipisicing do eu duis deserunt dolor aute labore dolore anim elit irure amet mollit. Est esse minim nisi labore incididunt. Proident excepteur aliquip dolore minim reprehenderit ut excepteur ipsum.'
-        ],
-        [
-            'title' => 'Blog ketiga',
-            'slug' => 'blog-ketiga',
-            'author' => 'Arif Budiman Arrosyid',
-            'body' => 'Tempor irure elit elit incididunt nulla deserunt consectetur Lorem. Commodo aute laboris aute in qui. Id anim proident est ex eiusmod fugiat mollit minim consectetur dolor. Ad laborum amet labore aute et mollit eiusmod aliquip veniam.
-
-            Deserunt amet sunt veniam nulla eu aute officia laboris irure dolor adipisicing. Culpa id magna reprehenderit culpa amet. Eu velit sunt consequat est reprehenderit veniam occaecat irure.
-
-            Eiusmod Lorem reprehenderit exercitation mollit aute mollit ex elit adipisicing eiusmod labore veniam ipsum. Culpa velit consectetur do labore culpa. Dolor irure laborum excepteur esse anim irure deserunt officia et. Laborum mollit eiusmod ad voluptate eu deserunt. Ipsum nisi dolore mollit occaecat.
-
-            Irure ullamco occaecat est eu occaecat consequat ipsum proident ea consequat quis aliquip. Elit commodo Lorem do do et ipsum aute duis eiusmod fugiat qui dolor deserunt. Eu pariatur amet officia velit ex sunt duis velit Lorem incididunt laborum non do veniam. Officia cupidatat laboris culpa consequat dolore et sint excepteur proident tempor qui sint. Exercitation esse laboris veniam reprehenderit adipisicing elit enim qui.
-
-            Commodo consectetur culpa veniam eu Lorem consequat et. Esse sunt exercitation deserunt sit quis amet officia elit labore laboris. Nulla ullamco quis Lorem labore elit duis laboris culpa ut consectetur ullamco amet laborum. Proident excepteur sunt ad velit voluptate.
-
-            Nisi tempor sit officia minim qui anim ex nostrud mollit aliquip quis. Fugiat ut Lorem exercitation proident fugiat elit nostrud deserunt sint Lorem adipisicing mollit dolor sint. Sit excepteur qui commodo ut quis. Aliquip exercitation consectetur sint consequat culpa cillum fugiat excepteur. Reprehenderit et esse proident tempor amet dolore duis et.
-
-            Est ad aliquip ullamco cupidatat sunt quis ex veniam eiusmod fugiat fugiat. Sit adipisicing reprehenderit nulla excepteur cupidatat. Veniam laborum ipsum ea fugiat ipsum do est irure velit sint. Nisi exercitation consequat ex do culpa non. Elit enim incididunt minim consequat anim duis tempor mollit pariatur dolor. Adipisicing Lorem mollit sint ipsum. Incididunt fugiat proident consectetur exercitation mollit sint pariatur nisi esse aliqua occaecat elit.'
-        ],
-    ];
-
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-    }
+    // protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id', 'created_at', 'updated_at'];
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,12 +5,21 @@
 
 <article class="mb-5">
 
-    <h2>{{ $post["title"] }}</h2>
-    <h5>By {{ $post["author"] }}</h5>
-    <p>{{ $post["body"] }}</p>
+    <h2>{{ $post->title }}</h2>
+    {{-- <h5>By {{ $post->author }}</h5> --}}
+    <p>{{ $post->excerpt }}</p>
+    {!!$post->body!!}
 </article>
 
 <a href="/blog">Back</a>
 
 
 @endsection
+
+{{-- Post::create([
+    'title' => 'Judul Post Kedua',
+    'slug' => 'judul-post-kedua',
+    'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+    'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>'
+]); --}}
+
