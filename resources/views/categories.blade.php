284274b97e854556a887e8b405c71f349cc19add
arifbudimanar <arifbudiman01041996@hotmail.com>
2022-10-12 18:58:25 +0700

    post category & eloquent relationship

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -1,4 +1,17 @@
 @extends('layouts.main')
 @section('container')
-<h1>Halaman Categries</h1>
+<h1 class="mb-5">Post Categories</h1>
+@foreach ($categories as $category)
+
+<article class="mb-5">
+    <h2>
+        <a href="/categories/{{ $category->slug }}">
+            {{ $category->name }}
+        </a>
+    </h2>
+
+</article>
+
+@endforeach
+
 @endsection
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,7 +6,7 @@
 <article class="mb-5">
 
     <h2>{{ $post->title }}</h2>
-    {{-- <h5>By {{ $post->author }}</h5> --}}
+    <h5>By Arif Budiman in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</h5></a>
     <p>{{ $post->excerpt }}</p>
     {!!$post->body!!}
 </article>
@@ -17,9 +17,14 @@
 @endsection
 
 {{-- Post::create([
-    'title' => 'Judul Post Kedua',
-    'slug' => 'judul-post-kedua',
-    'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
-    'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>'
+'title' => 'Judul Post Kedua',
+'slug' => 'judul-post-kedua',
+'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>',
+'category_id' => 1
 ]); --}}
 
+{{-- Category::create([
+'name' => 'Personal',
+'slug' => 'personal'
+]); --}}
