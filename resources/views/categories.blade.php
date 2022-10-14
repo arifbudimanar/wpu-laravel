193ce7527fbb7346b8ade9a89da838982506df9f
arifbudimanar <arifbudiman01041996@hotmail.com>
2022-10-15 01:32:58 +0700

    initial commit

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -5,19 +5,19 @@
 <div class="container">
     <div class="row">
         @foreach ($categories as $category)
-        <a href="/categories/{{ $category->slug }}">
-            <div class="col-md-4">
+        <div class="col-md-4">
+            <a href="/categories/{{ $category->slug }}">
                 <div class="card bg-dark text-white">
                     <img src="https://source.unsplash.com/500x400?{{ $category->name }}" class="card-img"
                         alt="{{ $category->name }}">
-                    <div class="card-img-overlay d-flex align-items-center p-0">
-                        <h5 class="card-title text-center flex-fill p-5 fs-3"
-                            style="background-color: rgba(0, 0, 0, 0.5)">{{ $category->name }}
+                    <div class="card-img-overlay d-flex align-items-center justify-content-center p-0">
+                        <h5 class="card-title text-center flex-fill p-4 fs-3"
+                            style="background-color: rgba(0, 0, 0, 0.579)">{{ $category->name }}
                         </h5>
                     </div>
                 </div>
-            </div>
-        </a>
+            </a>
+        </div>
         @endforeach
     </div>
 </div>
