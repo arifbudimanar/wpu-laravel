adc2a965d454d6f27cad2bc83dee105ee5494f36
arifbudimanar <arifbudiman01041996@hotmail.com>
2022-12-03 17:02:57 +0700

    initial commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -34,7 +34,7 @@ class PostController extends Controller
             //Lazy Loading
             // 'posts' => Post::latest()->get()
 
-            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->paginate('7')->withQueryString()
+            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(7)->withQueryString()
         ]);
     }
 
